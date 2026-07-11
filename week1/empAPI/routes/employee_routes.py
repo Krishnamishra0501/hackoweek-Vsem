e3e4519ba854bfe6380c8c4dbdf586406d7af403
Krishnamishra0501 <krishna.mishra11@yahoo.co.in>
2026-07-11 23:51:33 +0530

    Implemented Edit and Delete Employee features

--- week1/empAPI/routes/employee_routes.py
+++ week1/empAPI/routes/employee_routes.py
@@ -8,12 +8,6 @@ employee_bp = Blueprint("employee", __name__)
 @employee_bp.route("/employees")
 def employee_list():
     employees = Employee.query.all()
-
-    print("========== EMPLOYEES ==========")
-    print(employees)
-    print("Total Employees:", len(employees))
-    print("===============================")
-
     return render_template("employee_list.html", employees=employees)
 
 
@@ -40,3 +34,38 @@ def add_employee():
         return redirect(url_for("employee.employee_list"))
 
     return render_template("add_employee.html")
+
+
+@employee_bp.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit_employee(id):
+
+    employee = Employee.query.get_or_404(id)
+
+    if request.method == "POST":
+
+        employee.first_name = request.form["first_name"]
+        employee.last_name = request.form["last_name"]
+        employee.profession = request.form["profession"]
+        employee.department = request.form["department"]
+        employee.city = request.form["city"]
+        employee.branch = request.form["branch"]
+        employee.email = request.form["email"]
+        employee.phone = request.form["phone"]
+        employee.salary = float(request.form["salary"])
+
+        db.session.commit()
+
+        return redirect(url_for("employee.employee_list"))
+
+    return render_template("edit_employee.html", employee=employee)
+
+
+@employee_bp.route("/delete/<int:id>")
+def delete_employee(id):
+
+    employee = Employee.query.get_or_404(id)
+
+    db.session.delete(employee)
+    db.session.commit()
+
+    return redirect(url_for("employee.employee_list"))
